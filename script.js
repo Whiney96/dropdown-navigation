ba629a3b74c2e3d7be98673a517e06c8ef24ed7c
wineshuga <uziwinnie@gmail.com>
2022-10-12 14:49:37 +0100

    mobile menu bar completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// the first dropdown
+
 let dropdown1 = document.querySelector("#features");
 function show() {
     if (dropdown1.style.display == "block") {
@@ -10,6 +12,8 @@ function show() {
 }
 document.getElementById('features-btn').addEventListener('click', show)
 
+// the second dropdown
+
 const dropdown2 = document.querySelector('#company-dropdown');
 function handleClick() {
     if(dropdown2.style.display == 'block') {
@@ -21,3 +25,20 @@ function handleClick() {
     }
 }
 document.getElementById('company').addEventListener('click', handleClick)
+
+// the menu icon
+
+const menuIcon = document.querySelector('#menu-icon');
+const imgSrc = document.getElementById('menu-icon').src="/images/icon-menu.svg";
+const hiddenNav = document.getElementById('mobile-nav');
+
+function iconChange() {
+    if(hiddenNav.style.display == 'none') {
+        hiddenNav.style.display = 'block';
+        document.getElementById('menu-icon').src="/images/icon-close-menu.svg";
+    } else {
+        hiddenNav.style.display = 'none';
+        document.getElementById('menu-icon').src="/images/icon-menu.svg";
+    }
+}
+document.getElementById('menu-icon').addEventListener('click', iconChange);
